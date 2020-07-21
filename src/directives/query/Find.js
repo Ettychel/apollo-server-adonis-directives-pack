9a3370eb1171305c999f1ba8caf825294e5db877
Ettychel <krip17@gmail.com>
2020-07-21 09:40:20 +0300

    fix find directive

--- src/directives/query/Find.js
+++ src/directives/query/Find.js
@@ -1,32 +1,37 @@
-const BaseDirective = require('../BaseDirective')
+const BaseDirective = require("../BaseDirective");
 
 class FindDirective extends BaseDirective {
-
   visitFieldDefinition(field) {
-    const Model = this._getModel(this)
-    this._argumentArr = ['id']
+    const Model = this._getModel(this);
+    this._argumentArr = ["id"];
 
-    this._checkArguments(field)
+    this._checkArguments(field);
 
     field.resolve = async function (_, { id }) {
-      const res = await Model.find(id)
-      return res.toJSON()
-    }
+      const res = await Model.find(id);
+      try {
+        return res.toJSON();
+      } catch {
+        return [];
+      }
+    };
   }
 
   _checkArguments(field) {
-    this._checkReqArgs(field)
+    this._checkReqArgs(field);
 
-    const kind = this._getTypeIgnoreNonNull(field.args.find(e => e.name === 'id').astNode.type)
-    if (kind !== 'NamedType') throw new Error('The id argument cannot be an array')
+    const kind = this._getTypeIgnoreNonNull(
+      field.args.find((e) => e.name === "id").astNode.type
+    );
+    if (kind !== "NamedType")
+      throw new Error("The id argument cannot be an array");
   }
-
 }
 
 const FindTypeDefs = `directive @find(
   # By default, the model will be calculated from the name
   # of the returned "type" with the prefix 'App/Model/'
   model: String
-  ) on FIELD_DEFINITION`
+  ) on FIELD_DEFINITION`;
 
-module.exports = { FindDirective, FindTypeDefs }
+module.exports = { FindDirective, FindTypeDefs };
