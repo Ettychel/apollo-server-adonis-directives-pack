8667399bfbdce41e8b4a985056102923713229e2
Ettychel <krip17@gmail.com>
2020-07-23 17:43:15 +0300

    fix serialized createMany directive

--- src/directives/mutation/CreateMany.js
+++ src/directives/mutation/CreateMany.js
@@ -1,34 +1,41 @@
-const BaseDirective = require('../BaseDirective')
+const BaseDirective = require("../BaseDirective");
 
 class CreateManyDirective extends BaseDirective {
-
   visitFieldDefinition(field) {
-    const Model = this._getModel(this)
-    this._argumentArr = ['input']
+    const Model = this._getModel(this);
+    this._argumentArr = ["input"];
 
-    this._checkArguments(field)
+    this._checkArguments(field);
 
     field.resolve = async function (item, { input }) {
-      const res = await Model.createMany(input)
-      res.map(e => e.toJSON())
-      return res
-    }
+      let res = await Model.createMany(input);
+      res = res.map((e) => e.toJSON());
+      return res;
+    };
   }
 
   _checkArguments(field) {
-    this._checkReqArgs(field)
+    this._checkReqArgs(field);
 
-    const kind = this._getTypeIgnoreNonNull(field.args.find(e => e.name === 'input').astNode.type)
-    if (kind !== 'ListType') throw new Error('The @createMany directive only accepts an array of models')
-    
-    const returnType = this._getTypeIgnoreNonNull(field.astNode.type)
-    if (returnType !== 'ListType') throw new Error('The @createMany directive only returns an array of models')
+    const kind = this._getTypeIgnoreNonNull(
+      field.args.find((e) => e.name === "input").astNode.type
+    );
+    if (kind !== "ListType")
+      throw new Error(
+        "The @createMany directive only accepts an array of models"
+      );
+
+    const returnType = this._getTypeIgnoreNonNull(field.astNode.type);
+    if (returnType !== "ListType")
+      throw new Error(
+        "The @createMany directive only returns an array of models"
+      );
   }
 }
 
 const CreateManyTypeDefs = `directive @createMany(
   # By default, the model will be calculated from the name
   # of the returned "type" with the prefix 'App/Model/'
-  model: String) on FIELD_DEFINITION`
+  model: String) on FIELD_DEFINITION`;
 
-module.exports = { CreateManyDirective, CreateManyTypeDefs }
+module.exports = { CreateManyDirective, CreateManyTypeDefs };
